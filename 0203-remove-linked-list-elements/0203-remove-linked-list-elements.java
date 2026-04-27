8e40b0f7310e8547d0c09cb1ccffeb225ff34738
Pushpendu Das <pushpendudas02@gmail.com>
2026-04-27 20:03:39 +0530

    Time: 1 ms (95.66%), Space: 47.1 MB (90.84%) - LeetHub

--- 0203-remove-linked-list-elements/0203-remove-linked-list-elements.java
+++ 0203-remove-linked-list-elements/0203-remove-linked-list-elements.java
@@ -10,20 +10,25 @@
  */
 class Solution {
     public ListNode removeElements(ListNode head, int val) {
-        if(head==null){
-            return head;
-        }
-      while(head!=null&&head.val==val){
-        head=head.next;
-      }
-      ListNode curr=head;
-      while(curr!=null&&curr.next!=null){
-        if(curr.next.val==val){
-            curr.next=curr.next.next;
-        }else{
-            curr=curr.next;
-        }
-      }
-      return head;
+    //     if(head==null){
+    //         return head;
+    //     }
+    //   while(head!=null&&head.val==val){
+    //     head=head.next;
+    //   }
+    //   ListNode curr=head;
+    //   while(curr!=null&&curr.next!=null){
+    //     if(curr.next.val==val){
+    //         curr.next=curr.next.next;
+    //     }else{
+    //         curr=curr.next;
+    //     }
+    //   }
+    //   return head;
+    if(head==null){
+        return null;
+    }
+    head.next=removeElements(head.next,val);
+    return head.val==val?head.next:head;
     }
 }
