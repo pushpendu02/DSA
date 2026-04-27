efff766f533f55bdf9c7006f0e8417ab16be16a6
Pushpendu Das <pushpendudas02@gmail.com>
2026-04-27 20:04:02 +0530

    Time: 0 ms (100%), Space: 47.2 MB (61.58%) - LeetHub

--- 0203-remove-linked-list-elements/0203-remove-linked-list-elements.java
+++ 0203-remove-linked-list-elements/0203-remove-linked-list-elements.java
@@ -25,9 +25,7 @@ class Solution {
     //     }
     //   }
     //   return head;
-    if(head==null){
-        return null;
-    }
+    if(head==null)return null;
     head.next=removeElements(head.next,val);
     return head.val==val?head.next:head;
     }
