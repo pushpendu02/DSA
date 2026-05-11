001447311236ee1c9ccbc16a3028d1e2981aa787
Pushpendu Das <pushpendudas02@gmail.com>
2026-05-11 20:58:55 +0530

    Time: 1 ms (99.97%), Space: 42.4 MB (90.03%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int reverse(int x) {
+        int remainder;
+        int reverse=0;
+        while(x!=0){
+            remainder=x%10;
+            if(reverse>Integer.MAX_VALUE/10||reverse<Integer.MIN_VALUE/10){
+            return 0;
+        }
+            reverse=reverse*10+remainder;
+            x=x/10;
+        }
+        
+            
+            return reverse;
+        
+    }
+}
