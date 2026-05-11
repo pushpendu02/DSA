814dc40e26fea270cce767d5953337093aa583ae
Pushpendu Das <pushpendudas02@gmail.com>
2026-05-11 21:00:14 +0530

    Time: 0 ms (100%), Space: 43.4 MB (11.1%) - LeetHub

--- 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.java
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.java
@@ -3,7 +3,7 @@ class Solution {
         int n = nums.length;
         int idx = -1;
 
-        // 1. Find rotation index (where order breaks)
+        //Find rotation index (where order breaks)
         for(int i = 0; i < n - 1; i++){
             if(nums[i] > nums[i + 1]){
                 idx = i + 1;
@@ -14,13 +14,13 @@ class Solution {
         // If no break → already sorted
         if(idx == -1) return true;
 
-        // 2. Rotate array
+        // Rotate array
         int[] b = new int[n];
         for(int i = 0; i < n; i++){
             b[i] = nums[(i + idx) % n];
         }
 
-        // 3. Check if rotated array is sorted
+        // Check if rotated array is sorted
         for(int i = 0; i < n - 1; i++){
             if(b[i] > b[i + 1]){
                 return false;
