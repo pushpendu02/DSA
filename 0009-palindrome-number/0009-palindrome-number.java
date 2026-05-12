e287953e050aee27ced207fc174258dd92e3012a
Pushpendu Das <pushpendudas02@gmail.com>
2026-05-12 20:03:28 +0530

    Time: 5 ms (79.15%), Space: 45.9 MB (72.1%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,20 +1,15 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        int reverse=0;
-        int remainder;
-        int org=x;
-        if(x>0){
-        while(x!=0){
-            remainder=x%10;
-            reverse=reverse*10+remainder;
-            x=x/10;
-        }      
-        }
-        if(org==reverse){
-            return true;
-        }
-        else{
+        if(x < 0){
             return false;
         }
+        int original = x;
+        int reversed = 0;
+        while(x > 0){
+            int digit = x % 10;
+            reversed = reversed * 10 + digit;
+            x /= 10;
+        }
+        return original == reversed;
     }
 }
