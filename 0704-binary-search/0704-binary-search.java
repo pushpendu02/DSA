4a6f217f37e6e2232ecdee32dd9a0008ac5ce2aa
Pushpendu Das <pushpendudas02@gmail.com>
2026-05-27 21:14:47 +0530

    Time: 0 ms (100%), Space: 48.3 MB (49%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int search(int[] nums, int target) {
+       int low=0;
+       int high=nums.length-1;
+       int index;
+       while(low<=high){
+       int mid=(high+low)/2;
+        if(target==nums[mid]){
+            return mid;
+        }
+        if(target<nums[mid]){
+            high=mid-1;
+        }else{
+            low=mid+1;
+        }
+       } 
+    return -1;
+    }
+}
