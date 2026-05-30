37e98f869b718b15f02f24f38b06bec945e01676
Pushpendu Das <pushpendudas02@gmail.com>
2026-05-31 00:24:41 +0530

    Time: 2 ms (70.28%), Space: 55.6 MB (45.7%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -12,5 +12,17 @@ class Solution {
             }
         }
         return major;
+        // int freq=0,ans=0;
+        // for(int i=0;i<nums.length;i++){//Moore's voting algo
+        //     if(freq==0){
+        //         ans=nums[i];
+        //     }else if(ans==nums[i]){
+        //         freq++;
+        //     }else{
+        //         freq--;
+        //     }
+           
+        // }
+        //  return ans;
     }
 }
