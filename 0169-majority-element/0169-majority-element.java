88f87185676351c84e93a27cc1ba436e7b36f32f
Pushpendu Das <pushpendudas02@gmail.com>
2026-05-31 00:35:51 +0530

    Time: 1 ms (99.87%), Space: 55.5 MB (58.83%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,28 +1,28 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        int major=nums[0],count=1;
-        for(int i=1;i<nums.length;i++){
-            if(count==0){
-                count++;
-                major=nums[i];
-            }else if(major==nums[i]){
-                count++;
-            }else{
-                count--;
-            }
-        }
-        return major;
-        // int freq=0,ans=0;
-        // for(int i=0;i<nums.length;i++){//Moore's voting algo
-        //     if(freq==0){
-        //         ans=nums[i];
-        //     }else if(ans==nums[i]){
-        //         freq++;
+        // int major=nums[0],count=1;
+        // for(int i=1;i<nums.length;i++){
+        //     if(count==0){
+        //         count++;
+        //         major=nums[i];
+        //     }else if(major==nums[i]){
+        //         count++;
         //     }else{
-        //         freq--;
+        //         count--;
         //     }
-           
         // }
-        //  return ans;
+        // return major;
+        int freq=0;int ans=0;
+        for(int i=0;i<nums.length;i++){//Moore's voting algo
+            if(freq==0){
+                ans=nums[i];
+            } if(ans==nums[i]){
+                freq++;
+            }else{
+                freq--;
+            }
+           
+        }
+         return ans;
     }
 }
