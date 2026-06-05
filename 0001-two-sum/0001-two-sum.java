fc259582147dcf70b6fa2f1058dcb461b9c21d3d
Pushpendu Das <pushpendudas02@gmail.com>
2026-06-05 21:35:50 +0530

    Time: 2 ms (99.39%), Space: 47.1 MB (39.21%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,14 +1,25 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        for (int i = 0; i < nums.length; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                    if(nums[i]+nums[j]==target){
-                    return new int[] { i, j };
-                }
+        // for (int i = 0; i < nums.length; i++) {
+        //     for (int j = i + 1; j < nums.length; j++) {
+        //             if(nums[i]+nums[j]==target){
+        //             return new int[] { i, j };
+        //         }
+        //     }
+        // }
+        // // If no valid pair is found, return an empty array instead of null
+        // return new int[] {};
+        Map<Integer,Integer>pairIdx=new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            int fir=nums[i];
+            int sec=target-fir;
+            if(pairIdx.containsKey(sec)){
+                return new int[]{i,pairIdx.get(sec)};
             }
+            pairIdx.put(fir,i);
+            
         }
-        // If no valid pair is found, return an empty array instead of null
-        return new int[] {};
+        return new int[]{};
         
     }
 }
