25246ce9953dbb912d4c4f2b107c1a3e085e6933
Pushpendu Das <pushpendudas02@gmail.com>
2026-08-02 20:31:46 +0530

    Time: 0 ms (100%), Space: 47.5 MB (20.21%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+       int n=nums.length;
+       int expsum=n*(n+1)/2;
+       int actsum=0;
+       for(int i=0;i<nums.length;i++){
+        actsum=actsum+nums[i];
+       }
+       return expsum-actsum;
+
+    }
+}
