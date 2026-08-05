a5968e0eb6f6ff74497cc4b54830a52062286201
Pushpendu Das <pushpendudas02@gmail.com>
2026-08-05 20:08:23 +0530

    Time: 1 ms (99.95%), Space: 77.5 MB (14.28%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int cursum=0,maxsum=Integer.MIN_VALUE;
+        for(int i=0;i<nums.length;i++){
+            cursum=cursum+nums[i];
+            maxsum=Math.max(maxsum,cursum);
+            if(cursum<0){
+                cursum=0;
+            }
+        }
+        return maxsum;
+    }
+}
