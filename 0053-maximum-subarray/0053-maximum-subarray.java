2e1d4fdfc3c4c3fab832f7b033259ddfd28b3d65
Pushpendu Das <pushpendudas02@gmail.com>
2026-08-05 20:17:07 +0530

    Time: 1 ms (99.95%), Space: 77 MB (83.58%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -1,9 +1,18 @@
 class Solution {
     public int maxSubArray(int[] nums) {
+        // int maxsum=Integer.MIN_VALUE;
+        // for(int st=0;st<nums.length;st++){
+        //     int cursum=0;
+        //     for(int end=st;end<nums.length;end++){//This is bruteforce method
+        //         cursum=cursum+nums[end];
+        //         maxsum=Math.max(maxsum,cursum);
+        //     }
+        // }
+        // return maxsum;
         int cursum=0,maxsum=Integer.MIN_VALUE;
         for(int i=0;i<nums.length;i++){
             cursum=cursum+nums[i];
-            maxsum=Math.max(maxsum,cursum);
+            maxsum=Math.max(maxsum,cursum);//kadan's algo-when the sum is neg then it set again 0 because neg number never give the max sum
             if(cursum<0){
                 cursum=0;
             }
