b6da3b2355a8fb99ddcccb244d5ba6501bc0efb0
Pushpendu Das <pushpendudas02@gmail.com>
2026-09-07 21:38:08 +0530

    Time: 0 ms (100%), Space: 46.9 MB (28.74%) - LeetHub

---
+++ 0867-transpose-matrix/0867-transpose-matrix.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[][] transpose(int[][] matrix) {
+        
+       int n=matrix.length;
+       int m=matrix[0].length;
+        int newmat[][]=new int[m][n];
+       for(int i=0;i<m;i++){
+        for(int j=0;j<n;j++){
+            newmat[i][j]=matrix[j][i];
+        }
+       } 
+       return newmat;
+    }
+}
